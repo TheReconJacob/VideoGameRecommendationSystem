f2fe445f46164d46b3a3095206f5273a4b53ea53
thereconjacob <jvpokeyjake@gmail.com>
2023-06-07 16:14:34 +0100

    Add std namespace and populate recommendationsystem function

--- RecommendationSystem.cpp
+++ RecommendationSystem.cpp
@@ -1,4 +1,7 @@
 #include "RecommendationSystem.h"
+#include <algorithm>
+
+using namespace std;
 
 void RecommendationSystem::addUser(User user) {
     users[user.name] = user;
@@ -8,7 +11,56 @@ void RecommendationSystem::addGame(Game game) {
     games[game.name] = game;
 }
 
-std::vector<Game> RecommendationSystem::getRecommendations(User user) {
-    // Implement recommendation algorithm
-    return {};
+vector<Game> RecommendationSystem::getRecommendations(User user) {
+    vector<Game> recommendations;
+    unordered_map<string, int> genreFrequency;
+
+    // Count the number of times each genre appears in the user's history
+    for (const Game& game : user.history) {
+        for (const string& genre : game.genres) {
+            ++genreFrequency[genre];
+        }
+    }
+
+    // Find games that match the user's preferences and gather genres that appear frequently in their history of previously played games
+    for (const pair<string, Game>& gamePair : games) {
+        const Game& game = gamePair.second;
+        bool matchesPreferences = false;
+        //Calculates the the sum of the amount of times each genre in a game has occured in the user's history
+        int totalGenreFrequency = 0;
+
+        for (const string& genre : game.genres) {
+            if (find(user.preferences.begin(), user.preferences.end(), genre) != user.preferences.end()) {
+                matchesPreferences = true;
+            }
+            if (genreFrequency.count(genre) > 0) {
+                totalGenreFrequency += genreFrequency.at(genre);
+            }
+        }
+
+        if (matchesPreferences && totalGenreFrequency > 0) {
+            recommendations.push_back(game);
+        }
+    }
+
+    // Sort the recommendations by their total genre frequency
+    sort(recommendations.begin(), recommendations.end(), [&](const Game& gameA, const Game& gameB) {
+        int gameATotalGenreFrequency = 0;
+        int gameBTotalGenreFrequency = 0;
+
+        for (const string& genre : gameA.genres) {
+            if (genreFrequency.count(genre) > 0) {
+                gameATotalGenreFrequency += genreFrequency.at(genre);
+            }
+        }
+        for (const string& genre : gameB.genres) {
+            if (genreFrequency.count(genre) > 0) {
+                gameBTotalGenreFrequency += genreFrequency.at(genre);
+            }
+        }
+
+        return gameATotalGenreFrequency > gameBTotalGenreFrequency;
+    });
+
+    return recommendations;
 }
